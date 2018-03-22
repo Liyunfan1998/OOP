303cc1259718fabb5bf5e43e0ef8b157fdc0a014
liyunfan1998 <16302010002@fudan.edu.cn>
2018-03-22 10:32:57 +0800

    to be continued

--- src/Movebox.java
+++ src/Movebox.java
@@ -5,13 +5,15 @@ import java.util.*;
 
 /**
  * Created by luke1998 on 2018/3/19.
- * 还有一个box不能覆盖box的检查没做
+ * 2018-03-20   还有一个box不能覆盖box的检查没做
+ * 2018-03-22   现在做的map都是n*n，要改成n*m
+ * 2018-03-22   改成允许人移动到终点，人离开以后还要返回dest
  */
 public class Movebox {
 
     public static void main(String[] args) throws IOException {
         Movebox movebox = new Movebox();
-        String fileName = "./maps/2.map";
+        String fileName = "./maps/6.map";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
         int[] size = Map.getMapSize(bufferedReader, 2);
         int n = size[0];
